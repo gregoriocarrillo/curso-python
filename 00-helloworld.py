1b1ecc4f432165974c9ba59d41705e38c59d4b2a
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2023-05-16 10:44:22 -0400

    update files

---
+++ 00-helloworld.py
@@ -0,0 +1,30 @@
+# Esto es eun comentario de una linea
+print("Hola Python")
+
+"""
+esto es un comentario
+de varias
+lineas
+"""
+
+# Consultar tipo de datos
+
+print(type("Soy una cadena de texto")) #Tipo str
+print(type(10)) # Tipo int o integer
+print(type(10.5)) # Tipo float o decimal
+print(type(True)) #Tipo bool / True o False
+
+# Operaciones basicas
+print(4 + 3)
+print(4 - 3)
+print(4 * 3)
+print(4 % 3)
+print(4 / 3)
+print(4 ** 3)
+print(4 // 3)
+
+print(type(10))
+print(type(9.8))
+print(type(4 - 4j))
+print(type('Greg Carrillo'))
+print(type(['Greg', 'Python', 'Caracas']))
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # curso-python
+
+Basado en el curso de MoureDev y del repositorio 
+https://github.com/Asabeneh/30-Days-Of-Python
