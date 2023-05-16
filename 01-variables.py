25998daeb45c460207288e5ac3396a388770c856
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2023-05-16 09:32:42 -0400

    first commit

---
+++ 01-variables.py
@@ -0,0 +1,24 @@
+# Variables
+# Se deben nombrar en minusculas y en forma snake case usando _ para separar palabras
+
+my_string_variable = "Variable de string"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_int_variable = 10
+print(my_int_variable)
+print(type(my_int_variable))
+
+my_bool_variable = True
+print(my_bool_variable)
+print(type(my_bool_variable))
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+# Concatenacion de variables
+print(my_string_variable, my_int_variable, my_bool_variable)
+
+# Funciones del sistema
+print(len(my_string_variable)) # len() indica la longitud de caracteres de la variable
