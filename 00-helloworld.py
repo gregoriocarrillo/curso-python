2d4b002b53eb2a837a713eb509bf2252b90c17d3
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2023-05-17 13:15:10 -0400

    edicion y nuevo archivo

--- 00-helloworld.py
+++ 00-helloworld.py
@@ -12,6 +12,7 @@ lineas
 print(type("Soy una cadena de texto")) #Tipo str
 print(type(10)) # Tipo int o integer
 print(type(10.5)) # Tipo float o decimal
+print(type(4 - 4j)) # Tipo complex
 print(type(True)) #Tipo bool / True o False
 
 # Operaciones basicas
@@ -22,9 +23,3 @@ print(4 % 3)
 print(4 / 3)
 print(4 ** 3)
 print(4 // 3)
-
-print(type(10))
-print(type(9.8))
-print(type(4 - 4j))
-print(type('Greg Carrillo'))
-print(type(['Greg', 'Python', 'Caracas']))
--- 01-variables.py
+++ 01-variables.py
@@ -19,6 +19,27 @@ print(type(my_int_to_str_variable))
 
 # Concatenacion de variables
 print(my_string_variable, my_int_variable, my_bool_variable)
+print("Este es el valor de: ", my_bool_variable)
 
 # Funciones del sistema
 print(len(my_string_variable)) # len() indica la longitud de caracteres de la variable
+
+# Variables en una sola linea
+name, surname, alias, age = "Gregorio", "Carrillo", "gregcarrillo", 45
+print(name, surname, alias, age)
+
+# Inputs
+'''
+name = input("¿Cual es tu nombre? ")
+age = input("¿Que edad tienes? ")
+
+print(name)
+print(age)
+'''
+
+# Cambiar su tipo
+name = 45
+age = "Gregorio"
+print(name)
+print(age)
+
