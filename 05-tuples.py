867bcb90b50714f8fefba607d70271bc772cdf4a
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2023-06-04 17:45:14 -0400

    capitulo tuples completo

--- 05-tuples.py
+++ 05-tuples.py
@@ -1,8 +1,30 @@
 ### Tuples ###
-# es un conjunto de valores
+# es un conjunto de valores, es inmutable
 
 my_tuple = tuple()
 my_other_tuple = ()
 
 my_tuple = (45, 1.77, "Greg", "Carrillo")
 print(my_tuple)
+
+my_other_tuple = (35, 60, 30)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+
+print(my_tuple.count(45))
+print(my_tuple.index("Greg"))
+
+#my_tuple[1] = 1.80 TypeError: 'tuple' object does not support item assignment
+
+my_sum_tuple = my_tuple + my_other_tuple
+print(my_sum_tuple)
+print(type(my_sum_tuple))
+
+print(my_sum_tuple[3:6])
+
+my_tuple = list(my_tuple) # se reasigna su tipo, se convirtio a lista, para poder modificarla
+print(type(my_tuple))
+
+del my_tuple #del trabaja con variables
+#print(my_tuple) NameError: name 'my_tuple' is not defined
