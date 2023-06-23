f09b686a4b136194c4c80525d7c7ff3f22370492
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2023-06-22 22:41:09 -0400

    update cap dates

--- Intermedio/00_dates.py
+++ Intermedio/00_dates.py
@@ -19,4 +19,23 @@ print_date(now)
 year_2024 = datetime(2024, 1, 1)
 
 print_date(year_2024)
+'''
+EL FAMOSO "FIZZ BUZZ"
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+'''
 
+def fizzbuzz ():
+    for index in range(1, 101):
+        if index % 3 == 0 and index % 5 == 0:
+            print("fizzbuzz")
+        elif index % 3 == 0:
+            print("fizz")
+        elif index % 5 == 0:
+            print("buzz")
+        else:
+            print(index)
+
+fizzbuzz()
