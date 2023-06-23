4026e2c5331ff36b32283164c92e220e9d5851ac
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2023-06-23 18:03:57 -0400

    udpate intermedio

--- Intermedio/00_dates.py
+++ Intermedio/00_dates.py
@@ -2,40 +2,62 @@
 
 from datetime import datetime
 
-now = datetime.now()
+hoy = datetime.now() # se define variable con el modulo y la funcion interna now, variable definida por el modulo datetime
 
-def print_date(date):
-    print(date.year)
-    print(date.month)
-    print(date.day)
-    print(date.hour)
-    print(date.minute)
-    print(date.second)
-    print(date.timestamp())
+def imprime_fecha(fecha): # date como parametro
+    print(fecha.year)
+    print(fecha.month)
+    print(fecha.day)
+    print(fecha.hour)
+    print(fecha.minute)
+    print(fecha.second)
+    print(fecha.timestamp())
 
-print_date(now)
+imprime_fecha(hoy) # se llama a la funcion y como parametro la variable fecha_actual
 
+year_2024 = datetime(2024, 1, 1) # se crea variable con una fecha definida, como minimo año, mes y dia
+imprime_fecha(year_2024) # se invoca la funcion y como parametro la variable, se imprimira lo que contenga la funcion, se reusa la funcion con diferentes parametros
 
-year_2024 = datetime(2024, 1, 1)
+from datetime import time
 
-print_date(year_2024)
-'''
-EL FAMOSO "FIZZ BUZZ"
- * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
- * - Múltiplos de 3 por la palabra "fizz".
- * - Múltiplos de 5 por la palabra "buzz".
- * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
-'''
+current_time = time(13, 18, 0) # se define la hora
 
-def fizzbuzz ():
-    for index in range(1, 101):
-        if index % 3 == 0 and index % 5 == 0:
-            print("fizzbuzz")
-        elif index % 3 == 0:
-            print("fizz")
-        elif index % 5 == 0:
-            print("buzz")
-        else:
-            print(index)
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
 
-fizzbuzz()
+from datetime import date
+
+current_date = date.today() # modulo que solo muestra la fecha, variable que se define a raiz del modulo date
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(2023, 6, 23) # se puede definir la fecha
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+# Operaciones con fechas
+
+current_date = date(current_date.year, current_date.month + 1, current_date.day)
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+# se pueden realizar operaciones con objetos del mismo modulo
+diff = year_2024 - hoy
+print(diff)
+
+diff = year_2024.date() - current_date
+print(diff)
+
+from datetime import timedelta
+
+init_time_delta = timedelta(200, 100, 200, weeks = 10)
+end_time_delta = timedelta(300, 200, 100, weeks = 3)
+
+print(end_time_delta - init_time_delta)
+print(end_time_delta + init_time_delta)
