f371d8c37de507c9b274b31adca62803d9fceba3
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2023-06-22 22:22:41 -0400

    inicio cap dates

--- Intermedio/00_dates.py
+++ Intermedio/00_dates.py
@@ -1,2 +1,22 @@
 ### Dates ###
 
+from datetime import datetime
+
+now = datetime.now()
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+    print(date.timestamp())
+
+print_date(now)
+
+
+year_2024 = datetime(2024, 1, 1)
+
+print_date(year_2024)
+
